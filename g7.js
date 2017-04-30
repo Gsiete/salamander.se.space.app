92fe504915bc880d39b7a5bc479fac59ed3e6364
Gabriel Poussif <gpoussif@gmail.com>
2017-04-30 13:03:49 +0200

    ADD: even more

--- g7.js
+++ g7.js
@@ -1,16 +1,50 @@
+scenarios = {
+    0: {
+        0:true,
+        1:false,
+        2:true,
+        3:false,
+        4:false,
+        5:true
+    },
+    1: {
+        0:true,
+        1:false,
+        2:true,
+        3:true,
+        4:true,
+        5:false
+    }
+};
+layersInScenario = {
+    0: {
+        0:0,
+        1:2,
+        2:5,
+        3:false
+    },
+    1: {
+        0:0,
+        1:2,
+        2:3,
+        3:4
+    }
+};
 $(document).ready(function () {
     var layerSet = 0,
         layersPerSet = 3;
     $.each(project.layers, function (k, layer, o) {
-        layer.setVisible(Math.floor(k/layersPerSet) === parseInt(0))
+        layer.setVisible(scenarios[layerSet][k])
     });
     $('[name=layer]').on('change', function () {
-            project.layers[layersPerSet * layerSet + parseInt(this.value)].setVisible(this.checked);
+        if (layersInScenario[layerSet][this.value]) {
+            project.layers[layersInScenario[layerSet][this.value]].setVisible(this.checked);
+        }
     });
     $('[name=switch_layer_set]').on('change', function () {
         var $self = this;
         $.each(project.layers, function (k, layer, o) {
-            layer.setVisible(Math.floor(k/layersPerSet) === parseInt($self.value))
+            layer.setVisible(scenarios[$self.value][k])
         });
     })
 });
